c246d190dbaad43d914cd262a34a9919cad5fe78
James Byrne <jambyr@bas.ac.uk>
2025-03-26 16:30:01 +0000

    Capture sublogging

--- scripts/prep_cmip_training_data.sh
+++ scripts/prep_cmip_training_data.sh
@@ -6,12 +6,13 @@ conda activate $ICENET_CONDA
 set -o pipefail
 set -eu
 
-if [ $# -lt 1 ] || [ "$1" == "-h" ]; then
-    echo "Usage $0 <hemisphere> [download=0|1]"
+if [ $# -lt 2 ] || [ "$1" == "-h" ]; then
+    echo "Usage $0 <hemisphere> [osisaf|amsr] [download=0|1]"
     exit 1
 fi
 
 HEMI="$1"
+SIC_TYPE="$2"
 DOWNLOAD=${2:-0}
 
 # download-toolbox integration
@@ -23,7 +24,7 @@ if [ $DOWNLOAD -eq 1 ]; then
       echo "MEMBER: $MEMBER"
       COMMAND="download_cmip $DATA_ARGS --source $SOURCE --member $MEMBER $HEMI $CMIP6_DATES $CMIP6_VAR_ARGS"
       echo -e "\n\n$COMMAND\n\n"
-      $COMMAND >logs/download.cmip_${HEMI}.${SOURCE}.${MEMBER}.log 2>&1
+      $COMMAND 2>&1 | tee logs/download.cmip_${HEMI}.${SOURCE}.${MEMBER}.log
     done
   done 2>&1 | tee logs/download.cmip_${HEMI}.log
 fi
