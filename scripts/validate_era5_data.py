7a4a4d8a2c8147c11ed3fa39613867382fd5c9b3
James Byrne <jambyr@bas.ac.uk>
2025-03-28 22:48:41 +0000

    Adding quick hack script for ERA5 validation

---
+++ scripts/validate_era5_data.py
@@ -0,0 +1,32 @@
+import datetime as dt
+import glob
+import logging
+import os
+
+import pandas as pd
+import xarray as xr
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    logging.getLogger("gribapi.bindings").setLevel(logging.WARN)
+
+    var_dirs = []
+    for hemi in ["north", "south"]:
+        var_dirs.extend(glob.glob(os.path.join("data/era5/month", hemi, "*")))
+        for src_dirs in glob.glob(os.path.join("data/era5/*")):
+            if src_dirs.split(os.sep)[-1] in ["day", "month"]:
+                continue
+            var_dirs.append(os.path.join(src_dirs, hemi))
+
+    for dir in var_dirs:
+        logging.info("Directory: {}".format(dir))
+        dfs = glob.glob("{}/*.nc".format(dir))
+
+        logging.debug(dfs)
+        ds = xr.open_mfdataset(dfs)
+        da = getattr(ds, list(ds.data_vars)[0]).sum(["latitude", "longitude"]).compute()
+        da = da.where(da == 0., drop=True).squeeze().sortby(da.time)
+        year_files = set([pd.to_datetime(el).strftime("%Y.nc") for el in da.time.values])
+
+        for ydf in year_files:
+            print(os.path.join(dir, ydf))
