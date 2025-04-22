f58e3a506da247d1807751510af1a95850240b97
James Byrne <jambyr@bas.ac.uk>
2025-04-22 12:12:57 +0100

    Improve command logging isolation for clarity / traceability

--- scripts/pipeline_cmds.sh
+++ scripts/pipeline_cmds.sh
@@ -1,7 +1,11 @@
 function pipeline_run {
   COMMAND="$@"
+  SUFFIX=""
+
+  [ ! -z $DATA_FREQUENCY ] && [ ! -z $HEMI ] && SUFFIX="${DATA_FREQUENCY}.${HEMI}."
+
   echo -e "$COMMAND\n"
-  echo -e "$COMMAND\n" >>logs/commands.log
+  echo -e "$COMMAND\n" >>logs/commands.${SUFFIX}.log
   if [ ! ${DRY:+1} ] || [ $DRY -eq 0 ]; then
     $COMMAND
   fi
