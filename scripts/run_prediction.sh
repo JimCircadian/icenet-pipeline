3b5862d19dba3d6ff25ba7146f4feb65136318c0
James Byrne <jamescircadian@gmail.com>
2025-06-10 22:53:17 +0100

    Updating prediction script for new environments

--- scripts/run_prediction.sh
+++ scripts/run_prediction.sh
@@ -7,7 +7,7 @@ set -e -o pipefail
 conda activate $ICENET_CONDA
 
 if [ $# -lt 3 ] || [ "$1" == "-h" ]; then
-    echo "Usage $0 <prediction_name> <model> <hemisphere> [date_vars] [train_data_name]"
+    echo "Usage $0 <prediction_name> <model> <hemisphere>"
     echo "<prediction_name> name of prediction]"
     echo "<model>           model name"
     echo "<hemisphere>      hemisphere to use"
@@ -21,8 +21,8 @@ HEMI="$3"
 EXTRA_ARGS="${4:-""}"
 
 # This assumes you're not retraining using the same model name, eek
-if [ -d results/networks/${MODEL}.${HEMI} ]; then
-    SAVEFILE=`ls results/networks/${MODEL}.${HEMI}/${MODEL}.${HEMI}.*.h5 | head -n 1`
+if [ -d results/networks/${MODEL} ]; then
+    SAVEFILE=`ls results/networks/${MODEL}/${MODEL}*${HEMI}.*.h5 | head -n 1`
     DATASET=`echo $SAVEFILE | perl -lpe's/.+\.network_(.+)\.[0-9]+\.h5/$1/'`
     echo "First model file: $SAVEFILE"
     echo "Dataset model was trained on: $DATASET"
@@ -32,9 +32,7 @@ else
 fi
 
 LOADER_NAME="loader.${PREDICTION_NAME}.${HEMI}.json"
-jq -c '.sources[].splits["prediction"][]' $LOADER_NAME | sort | uniq | sed -r \
-    -e 's/"//g' \
-    -e 's/([0-9]{4})_([0-9]{2})_([0-9]{2})/\1-\2-\3/' >${PREDICTION_NAME}.${HEMI}.csv
+jq -r '.sources[].splits["prediction"][]' $LOADER_NAME | sort | uniq >${PREDICTION_NAME}.${HEMI}.csv
 
-./run_predict_ensemble.sh $EXTRA_ARGS -i $DATASET -f $FILTER_FACTOR -p $PREP_SCRIPT \
-    ${MODEL}.${HEMI} ${PREDICTION_NAME}.${HEMI} ${PREDICTION_NAME}.${HEMI} ${PREDICTION_NAME}.${HEMI}.csv
+echo ./scripts/run_predict_ensemble.sh $EXTRA_ARGS -i $DATASET -f $FILTER_FACTOR -p $PREP_SCRIPT \
+    ${MODEL} ${PREDICTION_NAME}.${HEMI} ${PREDICTION_NAME}.${HEMI} ${PREDICTION_NAME}.${HEMI}.csv
