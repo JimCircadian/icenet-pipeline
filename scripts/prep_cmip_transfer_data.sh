1fbb0666c028bd40dbe1549947e078722ee58236
James Byrne <jambyr@bas.ac.uk>
2025-06-24 11:44:42 +0100

    Backticks

--- scripts/prep_cmip_transfer_data.sh
+++ scripts/prep_cmip_transfer_data.sh
@@ -103,7 +103,7 @@ for SOURCE in ${!CMIP6_SOURCES[@]}; do
     pipeline_run icenet_dataset_create -v -c -p -ob $BATCH_SIZE -w $WORKERS -fl $FORECAST_LENGTH $LOADER_CONFIGURATION $DATASET_NAME
 
     if [ ! $DRY ]; then
-      LAG_DATE=${PLOT_DATE:-`cat ${LOADER_CONFIGURATION} | jq -r '.sources[.sources|keys[0]].splits.train[0]'}
+      LAG_DATE=${PLOT_DATE:-`cat ${LOADER_CONFIGURATION} | jq -r '.sources[.sources|keys[0]].splits.train[0]'`}
     else
       OFFSET=""
       if [ $DATA_FREQUENCY == "month" ]; then
