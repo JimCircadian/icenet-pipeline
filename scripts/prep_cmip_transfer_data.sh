7f317ea67742b93246cc5144cb3716d94d6889fa
James Byrne <jambyr@bas.ac.uk>
2025-06-26 12:26:59 +0100

    Correcting mistaken logic

--- scripts/prep_cmip_transfer_data.sh
+++ scripts/prep_cmip_transfer_data.sh
@@ -103,7 +103,7 @@ for SOURCE in ${!CMIP6_SOURCES[@]}; do
 
     pipeline_run icenet_dataset_create -v -c -p -ob $BATCH_SIZE -w $WORKERS -fl $FORECAST_LENGTH $LOADER_CONFIGURATION $DATASET_NAME
 
-    if [ ! $DRY ] && [ ! -f $LOADER_CONFIGURATION ]; then
+    if [ ! $DRY ] || [ -f $LOADER_CONFIGURATION ]; then
       LAG_DATE=${PLOT_DATE:-`cat ${LOADER_CONFIGURATION} | jq '.sources[.sources|keys[0]].splits.train[0]' | tr -d '"'`}
     else
       OFFSET=""
